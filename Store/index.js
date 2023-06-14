4ff328aeba1139739753740d8fb77dc43ce89aa8
mhammajdouline <mhammajdouline@gmail.com>
2023-06-14 12:31:34 -0400

    first commit

---
+++ Store/index.js
@@ -0,0 +1,38 @@
+
+
+class Makeup{ 
+constructor(concealer,mascara,lipgloss)
+    {
+        this.concealer = concealer;
+        this.mascara = mascara;
+        this.lipgloss = lipgloss;
+     }
+}
+
+const beauty = new Makeup('Nars','Dior','Fenty');
+console.log(beauty);
+
+class otherMakeup{ 
+    constructor(foundation,blush,powder)
+        {
+            this.concealer = foundation;
+            this.mascara = blush;
+            this.lipgloss = powder;
+         }
+    }
+    
+    const otherBeauty = new otherMakeup('Nars','Maybelline','Fenty');
+    console.log(otherBeauty);
+
+    class otherMakeup2{ 
+        constructor(contour,lipliner,eyeshadow)
+            {
+                this.concealer = contour;
+                this.mascara = lipliner;
+                this.lipgloss = eyeshadow;
+             }
+        }
+        
+        const otherBeauty2 = new otherMakeup2('Nars','Maybelline','Fenty');
+        console.log(otherBeauty2);
+        
